1580c6020e2ce6b1e0681bb86884d0f30ab40786
Nickygrv <nicole.rojas@ucb.edu.bo>
2024-11-13 17:36:44 -0400

    Cambios

--- bdd.js
+++ bdd.js
@@ -1,17 +1,32 @@
-import mysql from 'mysql';
+import express from 'express';
+import cors from 'cors-express'; // Mantén esta importación si utilizas ESM
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger-output.json' assert { type: 'json' };
 
-// Configuración de la conexión a la base de datos
-const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ferreteria'
-};
+import * as db from './bdd.js';  // Si `bdd` está en formato ESM
+import router from './routes.js';  // Si `routes` está en formato ESM
 
-// Función para obtener una nueva conexión
-const createConnection = () => {
-    return mysql.createConnection(dbConfig);
-};
+const app = express();
 
-// Exportación de la función para crear conexiones
-export { createConnection };
+// Configuración de Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+import './swagger.js'; // Ejecuta swagger.js para generar la documentación
+
+// Configuración de la documentación de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Habilitar CORS utilizando cors-express
+app.use(cors({
+    allowedOrigins: ['http://localhost:4200'],
+    allowedMethods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// Utilizar el enrutador
+app.use(router);
+
+// Inicio del servidor
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000');
+});
